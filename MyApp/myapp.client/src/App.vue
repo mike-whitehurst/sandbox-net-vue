db88dc02c0c07229b6feacda55d94ea16a8cc26a
mike-whitehurst <git@mike-whitehurst.co.uk>
2024-11-19 10:16:37 +0000

    new app

---
+++ MyApp/myapp.client/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+ 
+      <HelloWorld msg="You did it!" />
+
+</template>
+
+<style scoped>
+
+</style>
